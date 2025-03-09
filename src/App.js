d9fe99076208d47efee6698467498644935e56b3
PriyafullstackDevloper <156500553+PriyafullstackDevloper@users.noreply.github.com>
2025-03-09 09:22:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import './App.css';
-import Dashboard from "./component/DashboardPortfolio/Dashboard"; 
+import Dashboard from "./component/DashboardPortfolio/Dashboard";
+import ImdbDashboard from "./Movies/ImdbDashboard";
+import Table from "./Table/Table";
+import Actor from "./component/Actor/Actor";
+import CoinDashboard from "./Coins/CoinDashboard";
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+     
+     <Dashboard />
+     <ImdbDashboard />
+     <Table />
+     <Actor />
+     <CoinDashboard />
+     
+     
+    
+    
+    
+
+
+     
+
+
     </div>
   );
 }
